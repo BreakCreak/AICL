1dc2e40bcbbec21c8836b2bc0d647d7eed0d63c0
SteamPotter <12435745+steampotter@user.noreply.gitee.com>
2026-01-21 19:51:46 +0800

    优化

--- inference_thumos.py
+++ inference_thumos.py
@@ -1,130 +1,123 @@
 import os
 import json
 import torch
+import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
 from utils import misc_utils
-import torch.nn.functional as F
 from run_eval import evaluate
-import matplotlib.pyplot as plt
-import scipy.special
-import pickle
-from collections import defaultdict
 from config.config_thumos import class_dict
-from scipy.interpolate import interp1d
-
 
 def load_weight(model_file, net):
     if model_file is not None:
-        print("loading from file: ", model_file)
+        print("Loading model weights from:", model_file)
         net.load_state_dict(torch.load(model_file), strict=False)
-
 
 def inference(net, config, test_loader, model_file=None):
     np.set_printoptions(formatter={'float_kind': "{:.4f}".format})
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    net.eval()
 
-    with torch.no_grad():
-        net.eval()
+    # 加载权重
+    load_weight(model_file, net)
 
-        # load weights
-        load_weight(model_file, net)
+    final_res = {'version': 'VERSION 1.3', 'results': {},
+                 'external_data': {'used': True, 'details': 'Features from I3D Network'}}
 
-        final_res = {'version': 'VERSION 1.3', 'results': {},
-                     'external_data': {'used': True, 'details': 'Features from I3D Network'}}
+    num_correct = 0.
+    num_total = 0.
 
-        num_correct = 0.
-        num_total = 0.
+    for _data, _label, temp_anno, vid_name, vid_num_seg in tqdm(test_loader):
+        _data = _data.to(device)
+        _label = _label.to(device)
+        vid = vid_name[0]
 
-        for _data, _label, temp_anno, vid_name, vid_num_seg in test_loader:
-
-            batch_size = _data.shape[0]
-            vid = vid_name[0]
-            _data = _data.cuda()
-            _label = _label.cuda()
-
-            # FORWARD PASS
+        with torch.no_grad():
+            # forward
             cas, action_flow, action_rgb, contrast_pairs, contrast_pairs_r, contrast_pairs_f, actionness1, actionness2, aness_bin1, aness_bin2 = net(_data)
 
-            # 使用新的 instance_selection_function2 来融合更多分支
-            combined_cas = misc_utils.instance_selection_function2(torch.softmax(cas.detach(), -1),  # cas_r
-                                                      action_flow.unsqueeze(1).permute(0, 2, 1).detach(),  # cas_f
-                                                      action_flow.unsqueeze(1).permute(0, 2, 1),  # cas_flow
-                                                      action_rgb.unsqueeze(1).permute(0, 2, 1))  # cas_rgb
+            # ====================== 多分支融合 ======================
+            # 直接用 soft gate 融合四分支
+            emb_rgb = action_rgb.unsqueeze(1).permute(0,2,1)
+            emb_flow = action_flow.unsqueeze(1).permute(0,2,1)
+            emb_mixed1 = contrast_pairs_r['CA']  # 你可以改成实际 embedding
+            emb_mixed2 = contrast_pairs_f['CA']
 
-            _, topk_indices = torch.topk(combined_cas, config.num_segments // 8, dim=1)
+            # 使用新的 instance_selection_function2 或自定义融合
+            combined_cas = misc_utils.instance_selection_function2(
+                torch.softmax(cas.detach(), -1),    # CAS 主干
+                emb_flow.detach(),
+                emb_mixed1.detach(),
+                emb_rgb.detach()
+            )
 
-            # class prediction
+            # top-k 取片段
+            topk_num = config.num_segments // 8
+            _, topk_indices = torch.topk(combined_cas, topk_num, dim=1)
             cas_top = torch.gather(cas, 1, topk_indices)
             cas_top = torch.mean(cas_top, dim=1)
             score_supp = F.softmax(cas_top, dim=1)
 
             label_np = _label.cpu().numpy()
-            score_np = score_supp[0, :].cpu().numpy()
+            score_np = score_supp[0].cpu().numpy()
 
-            score_np[np.where(score_np < config.class_thresh)] = 0
-            score_np[np.where(score_np >= config.class_thresh)] = 1
-
-
+            # 二值化
+            score_np[score_np < config.class_thresh] = 0
+            score_np[score_np >= config.class_thresh] = 1
             if np.all(score_np == 0):
-                arg = np.argmax(score_supp[0, :].cpu().data.numpy())
+                arg = np.argmax(score_supp[0].cpu().data.numpy())
                 score_np[arg] = 1
 
             correct_pred = np.sum(label_np == score_np, axis=1)
-
             num_correct += np.sum((correct_pred == config.num_classes).astype(np.float32))
             num_total += correct_pred.shape[0]
 
-            pred = np.where(score_np > 0.2)[0]  # 使用固定阈值0.2
-
             # action prediction
+            pred = np.where(score_np > 0.2)[0]  # 固定阈值
             if len(pred) != 0:
+                # 融合四分支 CAS
                 cas_pred = combined_cas[0].cpu().numpy()[:, pred]
                 cas_pred = np.reshape(cas_pred, (config.num_segments, -1, 1))
                 cas_pred = misc_utils.upgrade_resolution(cas_pred, config.scale)
 
                 proposal_dict = {}
-
                 for t in range(len(config.act_thresh)):
                     cas_temp = cas_pred.copy()
-                    zero_location = np.where(cas_temp[:, :, 0] < config.act_thresh[t])
+                    zero_location = np.where(cas_temp[:,:,0] < config.act_thresh[t])
                     cas_temp[zero_location] = 0
-
-                    cas_input = cas_pred.copy()
-
                     seg_list = []
                     for c in range(len(pred)):
                         pos = np.where(cas_temp[:, c, 0] > 0)
                         seg_list.append(pos)
-
-                    proposals = misc_utils.get_proposal_oic(seg_list, cas_pred.copy(), score_supp[0, :].cpu().data.numpy(),
-                                                            pred, config.scale, vid_num_seg[0].cpu().item(), config.feature_fps,
-                                                            config.num_segments, config.gamma)
-
+                    proposals = misc_utils.get_proposal_oic(
+                        seg_list, cas_pred.copy(), score_supp[0].cpu().numpy(),
+                        pred, config.scale, vid_num_seg[0].cpu().item(),
+                        config.feature_fps, config.num_segments, config.gamma
+                    )
                     for j in range(len(proposals)):
                         if not proposals[j]:
                             continue
                         class_id = proposals[j][0][0]
-
-                        if class_id not in proposal_dict.keys():
+                        if class_id not in proposal_dict:
                             proposal_dict[class_id] = []
-
                         proposal_dict[class_id] += proposals[j]
 
+                # NMS
                 final_proposals = []
                 for class_id in proposal_dict.keys():
-                    final_proposals.append(misc_utils.basnet_nms(proposal_dict[class_id], config.nms_thresh,
-                                                                 config.soft_nms, config.nms_alpha))
+                    final_proposals.append(
+                        misc_utils.basnet_nms(proposal_dict[class_id], config.nms_thresh,
+                                               config.soft_nms, config.nms_alpha)
+                    )
+                final_res['results'][vid] = misc_utils.result2json(final_proposals)
 
-                final_res['results'][vid_name[0]] = misc_utils.result2json(final_proposals)
+    test_acc = num_correct / num_total
+    json_path = os.path.join(config.model_path, 'temp_result.json')
+    with open(json_path, 'w') as f:
+        json.dump(final_res, f)
 
-        test_acc = num_correct / num_total
-
-        json_path = os.path.join(config.model_path, 'temp_result.json')
-
-        with open(json_path, 'w') as f:
-            json.dump(final_res, f)
-            f.close()
-
-        mean_ap, _ = evaluate(config.gt_path, json_path, None, tiou_thresholds=np.linspace(0.1, 0.7, 7), plot=False,
-                                     subset='test', verbose=config.verbose)
-        return mean_ap, test_acc
+    mean_ap, _ = evaluate(config.gt_path, json_path, None,
+                          tiou_thresholds=np.linspace(0.1, 0.7, 7),
+                          plot=False, subset='test', verbose=config.verbose)
+    return mean_ap, test_acc
--- models/model.py
+++ models/model.py
@@ -1,11 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.nn.init as torch_init
-import math
 import numpy as np
 torch.set_printoptions(profile="full")
 
+# ===================== BaseModel =====================
 class BaseModel(nn.Module):
     def __init__(self, len_feature, num_classes, config=None):
         super(BaseModel, self).__init__()
@@ -13,173 +12,125 @@ class BaseModel(nn.Module):
         self.num_classes = num_classes
         self.config = config
 
-        self.base_module = nn.Sequential(
-            nn.Conv1d(in_channels=self.len_feature, out_channels=512, kernel_size=3, padding=1),
-            nn.ReLU(),
-        )
-
-        self.cls = nn.Sequential(
-            nn.Conv1d(in_channels=512, out_channels=self.num_classes, kernel_size=1, padding=0),
-        )
-
+        # RGB & Flow 分支
         self.action_module_rgb = nn.Sequential(
             nn.Conv1d(in_channels=self.len_feature // 2, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU(),
         )
-
-        self.cls_rgb = nn.Conv1d(in_channels=512, out_channels=1, kernel_size=1, padding=0)
+        self.cls_rgb = nn.Conv1d(512, 1, 1)
 
         self.action_module_flow = nn.Sequential(
             nn.Conv1d(in_channels=self.len_feature // 2, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU(),
         )
+        self.cls_flow = nn.Conv1d(512, 1, 1)
 
-        self.cls_flow = nn.Conv1d(in_channels=512, out_channels=1, kernel_size=1, padding=0)
-        
-        # 新增两个混合分支
+        # 混合分支
         self.action_module_mixed1 = nn.Sequential(
             nn.Conv1d(in_channels=self.len_feature // 2, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU(),
         )
-        self.cls_mixed1 = nn.Conv1d(in_channels=512, out_channels=1, kernel_size=1, padding=0)
+        self.cls_mixed1 = nn.Conv1d(512, 1, 1)
 
         self.action_module_mixed2 = nn.Sequential(
             nn.Conv1d(in_channels=self.len_feature // 2, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU(),
         )
-        self.cls_mixed2 = nn.Conv1d(in_channels=512, out_channels=1, kernel_size=1, padding=0)
+        self.cls_mixed2 = nn.Conv1d(512, 1, 1)
 
-        self.dropout = nn.Dropout(p=0.5)  # 0.5
-
-        # 多尺度时间卷积
-        self.temporal_convs = nn.ModuleList([
-            nn.Conv1d(512, 512, 3, padding=1),
-            nn.Conv1d(512, 512, 5, padding=2),
-            nn.Conv1d(512, 512, 7, padding=3)
-        ])
-        
-        # Actionness Head
-        self.actionness_head = nn.Sequential(
-            nn.Conv1d(512, 256, 1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(256, 1, 1),
-            nn.Sigmoid()
-        )
-
-        # 修改门控模块为4个通道，对应4个分支
+        # 门控模块
         self.gate_module = nn.Sequential(
-            nn.Conv1d(in_channels=self.len_feature, out_channels=512, kernel_size=3, padding=1),
+            nn.Conv1d(len_feature, 512, 3, padding=1),
             nn.ReLU(),
-            nn.Conv1d(in_channels=512, out_channels=4, kernel_size=1),  # 4个通道分别表示4个分支的权重
-            nn.Softmax(dim=1)  # 沿着通道维度做Softmax，得到权重
+            nn.Conv1d(512, 4, 1),
+            nn.Softmax(dim=1)
         )
 
-    def forward(self, x):
+        # 最终分类
+        self.cls = nn.Conv1d(512, self.num_classes, 1)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x, inference=False):
         input = x.permute(0, 2, 1)
 
-        # 提取RGB和Flow的特征
-        emb_flow = self.action_module_flow(input[:, 1024:, :])
+        # RGB & Flow 特征
         emb_rgb = self.action_module_rgb(input[:, :1024, :])
+        emb_flow = self.action_module_flow(input[:, 1024:, :])
 
-        # 提取混合分支的特征
-        emb_mixed1 = self.action_module_mixed1(0.25 * input[:, :1024, :] + 0.75 * input[:, 1024:, :])
-        emb_mixed2 = self.action_module_mixed2(0.75 * input[:, :1024, :] + 0.25 * input[:, 1024:, :])
+        # 混合分支
+        emb_mixed1 = self.action_module_mixed1(0.25*input[:, :1024, :] + 0.75*input[:, 1024:, :])
+        emb_mixed2 = self.action_module_mixed2(0.75*input[:, :1024, :] + 0.25*input[:, 1024:, :])
 
-        # 获取门控权重
-        gate_weights = self.gate_module(input)  # shape: [B, 4, T]
-        rgb_weight = gate_weights[:, 0:1, :]  # shape: [B, 1, T]
-        flow_weight = gate_weights[:, 1:2, :]  # shape: [B, 1, T]
-        mixed1_weight = gate_weights[:, 2:3, :]  # shape: [B, 1, T]
-        mixed2_weight = gate_weights[:, 3:4, :]  # shape: [B, 1, T]
+        # 门控
+        gate_weights = self.gate_module(input)
+        if inference:
+            # 软化门控
+            gate_weights = gate_weights * 0.7 + 0.3 / 4
 
-        # 对4个分支进行加权融合
-        emb = (0.5 * rgb_weight * emb_rgb +
-               0.5 * flow_weight * emb_flow +
-               0.75 * mixed1_weight * emb_mixed1 +
-               0.25 * mixed2_weight * emb_mixed2)
+        rgb_w, flow_w, m1_w, m2_w = gate_weights[:,0:1,:], gate_weights[:,1:2,:], gate_weights[:,2:3,:], gate_weights[:,3:4,:]
 
-        embedding_flow = emb_flow.permute(0, 2, 1)
-        embedding_rgb = emb_rgb.permute(0, 2, 1)
-        embedding = emb.permute(0, 2, 1)
+        # MoE 加权融合
+        emb = 0.5*rgb_w*emb_rgb + 0.5*flow_w*emb_flow + 0.75*m1_w*emb_mixed1 + 0.25*m2_w*emb_mixed2
 
-        # 分类输出
-        cas = self.cls(emb).permute(0, 2, 1)
-        actionness1 = cas.sum(dim=2)
-        actionness1 = torch.sigmoid(actionness1)
+        # 分类
+        cas = self.cls(emb).permute(0,2,1)
+        actionness1 = torch.sigmoid(cas.sum(dim=2))
 
-        # 单独计算各分支的动作性
-        action_flow = torch.sigmoid(self.cls_flow(emb_flow))
-        action_rgb = torch.sigmoid(self.cls_rgb(emb_rgb))
-        action_mixed1 = torch.sigmoid(self.cls_mixed1(emb_mixed1))
-        action_mixed2 = torch.sigmoid(self.cls_mixed2(emb_mixed2))
+        # 单分支 actionness
+        action_rgb = torch.sigmoid(self.cls_rgb(emb_rgb)).squeeze(1)
+        action_flow = torch.sigmoid(self.cls_flow(emb_flow)).squeeze(1)
+        action_mixed1 = torch.sigmoid(self.cls_mixed1(emb_mixed1)).squeeze(1)
+        action_mixed2 = torch.sigmoid(self.cls_mixed2(emb_mixed2)).squeeze(1)
 
-        action_flow = action_flow.squeeze(1)
-        action_rgb = action_rgb.squeeze(1)
-        action_mixed1 = action_mixed1.squeeze(1)
-        action_mixed2 = action_mixed2.squeeze(1)
+        # 融合 actionness2，保留低片段信息
+        low_action = torch.min(action_rgb, action_flow)
+        actionness2 = (0.6*action_mixed1 + 0.4*action_mixed2 + 0.5*action_rgb + 0.5*action_flow)/4
+        actionness2 = 0.8*actionness2 + 0.2*low_action
 
-        actionness2 = (action_flow + action_rgb + action_mixed1 + action_mixed2) / 4
+        return cas, action_flow, action_rgb, action_mixed1, action_mixed2, actionness1, actionness2, emb, emb_flow, emb_rgb
 
-        return cas, action_flow, action_rgb, actionness1, actionness2, embedding, embedding_flow, embedding_rgb
-
-
+# ===================== AICL =====================
 class AICL(nn.Module):
     def __init__(self, cfg):
         super(AICL, self).__init__()
         self.len_feature = 2048
         self.num_classes = 20
-
-        self.actionness_module = BaseModel(self.len_feature, self.num_classes, cfg)
-
-        self.softmax = nn.Softmax(dim=1)
-        self.softmax_2 = nn.Softmax(dim=2)
-
         self.r_C = 20
         self.r_I = 20
+        self.model = BaseModel(self.len_feature, self.num_classes, cfg)
+        self.dropout = nn.Dropout(0.6)
 
-        self.dropout = nn.Dropout(p=0.6)
-
-    def select_topk_embeddings(self, scores, embeddings, k):
+    def select_topk_embeddings(self, scores, embeddings, k, retain_random=0.2):
+        B, T, D = embeddings.shape
+        device = embeddings.device
         _, idx_DESC = scores.sort(descending=True, dim=1)
-        idx_topk = idx_DESC[:, :k]
-        idx_topk = idx_topk.unsqueeze(2).expand([-1, -1, embeddings.shape[2]])
-        selected_embeddings = torch.gather(embeddings, 1, idx_topk)
-        return selected_embeddings
+        k_top = int(k*(1-retain_random))
+        k_rand = k - k_top
+        idx_topk = idx_DESC[:, :k_top]
+        if k_rand > 0:
+            rand_idx = torch.randint(0, T, (B, k_rand), device=device)
+            idx_topk = torch.cat([idx_topk, rand_idx], dim=1)
+        idx_topk = idx_topk.unsqueeze(2).expand([-1, -1, D])
+        return torch.gather(embeddings, 1, idx_topk)
 
-    def consistency_snippets_mining1(self, aness_bin1, aness_bin2, actionness1, embeddings, k_easy):
-
+    def consistency_snippets_mining(self, aness_bin1, aness_bin2, actionness, embeddings, k_easy):
         x = aness_bin1 + aness_bin2
-        select_idx_act = actionness1.new_tensor(np.where(x == 2, 1, 0))
-        # print(torch.min(torch.sum(select_idx_act, dim=-1)))
-
-        actionness_act = actionness1 * select_idx_act
-
-        select_idx_bg = actionness1.new_tensor(np.where(x == 0, 1, 0))
-
-        actionness_rev = torch.max(actionness1, dim=1, keepdim=True)[0] - actionness1
-        actionness_bg = actionness_rev * select_idx_bg
-
-        easy_act = self.select_topk_embeddings(actionness_act, embeddings, k_easy)
-        easy_bkg = self.select_topk_embeddings(actionness_bg, embeddings, k_easy)
-
-
+        select_idx_act = actionness.new_tensor(np.where(x==2,1,0))
+        select_idx_bg = actionness.new_tensor(np.where(x==0,1,0))
+        easy_act = self.select_topk_embeddings(actionness*select_idx_act, embeddings, k_easy)
+        actionness_rev = torch.max(actionness,dim=1,keepdim=True)[0]-actionness
+        easy_bkg = self.select_topk_embeddings(actionness_rev*select_idx_bg, embeddings, k_easy)
         return easy_act, easy_bkg
 
-    def Inconsistency_snippets_mining1(self, aness_bin1, aness_bin2, actionness, embeddings, k_hard):
-
+    def inconsistency_snippets_mining(self, aness_bin1, aness_bin2, actionness, embeddings, k_hard):
         x = aness_bin1 + aness_bin2
-        idx_region_inner = actionness.new_tensor(np.where(x == 1, 1, 0))
-        # 引入actionness gating
-        actionness_gate = (actionness > 0.6).float()
-        aness_region_inner = actionness * idx_region_inner * actionness_gate
-        hard_act = self.select_topk_embeddings(aness_region_inner, embeddings, k_hard)
-
+        idx_region_inner = actionness.new_tensor(np.where(x==1,1,0))
+        gate_center, gate_scale = 0.4, 5.0
+        action_gate = torch.sigmoid((actionness - gate_center)*gate_scale)
+        hard_act = self.select_topk_embeddings(actionness*idx_region_inner*action_gate, embeddings, k_hard, retain_random=0.2)
         actionness_rev = torch.max(actionness, dim=1, keepdim=True)[0] - actionness
-        # 引入actionness gating
-        bg_actionness_gate = (actionness < 0.2).float()
-        aness_region_outer = actionness_rev * idx_region_inner + bg_actionness_gate * idx_region_inner
-        hard_bkg = self.select_topk_embeddings(aness_region_outer, embeddings, k_hard)
-
+        bg_gate = torch.sigmoid((0.3-actionness)*5)
+        hard_bkg = self.select_topk_embeddings(actionness_rev*idx_region_inner+bg_gate*idx_region_inner, embeddings, k_hard, retain_random=0.2)
         return hard_act, hard_bkg
 
     def forward(self, x):
@@ -187,46 +138,26 @@ class AICL(nn.Module):
         k_C = num_segments // self.r_C
         k_I = num_segments // self.r_I
 
-        cas, action_flow, action_rgb, actionness1, actionness2, embedding, embedding_flow, embedding_rgb = self.actionness_module(x)
+        cas, action_flow, action_rgb, action_mixed1, action_mixed2, actionness1, actionness2, embedding, embedding_flow, embedding_rgb = self.model(x)
 
         aness_np1 = actionness1.cpu().detach().numpy()
-        aness_median1 = np.median(aness_np1, 1, keepdims=True)
-        aness_bin1 = np.where(aness_np1 > aness_median1, 1.0, 0.0)
+        aness_bin1 = np.where(aness_np1 > np.median(aness_np1,1,keepdims=True),1.0,0.0)
 
         aness_np2 = actionness2.cpu().detach().numpy()
-        aness_median2 = np.median(aness_np2, 1, keepdims=True)
-        aness_bin2 = np.where(aness_np2 > aness_median2, 1.0, 0.0)
+        aness_bin2 = np.where(aness_np2 > np.median(aness_np2,1,keepdims=True),1.0,0.0)
 
-        # actionness = actionness1 + actionness2
+        # mining
+        CA, CB = self.consistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding, k_C)
+        IA, IB = self.inconsistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding, k_I)
 
-        CA, CB = self.consistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding, k_C)
-        IA, IB = self.Inconsistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding, k_I)
+        CAr, CBr = self.consistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding_rgb, k_C)
+        IAr, IBr = self.inconsistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding_rgb, k_I)
 
-        CAr, CBr = self.consistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding_rgb, k_C)
-        IAr, IBr = self.Inconsistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding_rgb, k_I)
+        CAf, CBf = self.consistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding_flow, k_C)
+        IAf, IBf = self.inconsistency_snippets_mining(aness_bin1, aness_bin2, actionness1, embedding_flow, k_I)
 
-        CAf, CBf = self.consistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding_flow, k_C)
-        IAf, IBf = self.Inconsistency_snippets_mining1(aness_bin1, aness_bin2, actionness1, embedding_flow, k_I)
+        contrast_pairs = {'CA': CA,'CB': CB,'IA': IA,'IB': IB}
+        contrast_pairs_r = {'CA': CAr,'CB': CBr,'IA': IAr,'IB': IBr}
+        contrast_pairs_f = {'CA': CAf,'CB': CBf,'IA': IAf,'IB': IBf}
 
-        contrast_pairs = {
-            'CA': CA,
-            'CB': CB,
-            'IA': IA,
-            'IB': IB
-        }
-
-        contrast_pairs_r = {
-            'CA': CAr,
-            'CB': CBr,
-            'IA': IAr,
-            'IB': IBr
-        }
-
-        contrast_pairs_f = {
-            'CA': CAf,
-            'CB': CBf,
-            'IA': IAf,
-            'IB': IBf
-        }
-
-        return cas, action_flow, action_rgb, contrast_pairs,contrast_pairs_r,contrast_pairs_f, actionness1, actionness2, aness_bin1, aness_bin2
+        return cas, action_flow, action_rgb, contrast_pairs, contrast_pairs_r, contrast_pairs_f, actionness1, actionness2, aness_bin1, aness_bin2
