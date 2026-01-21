4710f2866869f9f74b8d9147f2ac52b7548ceb43
SteamPotter <12435745+steampotter@user.noreply.gitee.com>
2026-01-21 19:32:23 +0800

    inference

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,6 +1,7 @@
 python main_thumos.py \
---exp_name AICL \
---model_name ThumosModel \
---soft_nms \
---inference_only \
---data_path DATA_PATH
+    --exp_name AICL \
+    --model_name ThumosModel \
+    --inference_only \
+    --checkpoint_path ./outputs/Thumos_1/CAS_Only.pkl \
+    --soft_nms \
+    --data_path '../THUMOS14'
